0937bbcc2ddda0a8348f80b794f911f136906ff9
Edward Minnett <ed@methodic.io>
2016-05-28 16:00:08 +0100

    Style: Reduce line length to fewer than 80 characters

--- spec/models/all_models_spec.rb
+++ spec/models/all_models_spec.rb
@@ -7,7 +7,8 @@ RSpec.describe 'All Models' do
   Rails.application.eager_load!
 
   required_attributes = [:created_at, :updated_at, :deleted_at]
-  user_created_models = ActiveRecord::Base.descendants - [ActiveRecord::SchemaMigration]
+  all_models          = ActiveRecord::Base.descendants
+  user_created_models = all_models - [ActiveRecord::SchemaMigration]
 
   user_created_models.each do |model|
     describe model do
