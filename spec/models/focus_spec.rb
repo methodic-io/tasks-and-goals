6850b1fd271419a1146c8d71344aa21fdf1844b7
Edward Minnett <ed@methodic.io>
2016-05-29 10:43:51 +0100

    Test: Add tests to the Focus spec
    Connects to #19
    Connects to #12

--- spec/models/focus_spec.rb
+++ spec/models/focus_spec.rb
@@ -4,7 +4,32 @@
 require 'rails_helper'
 
 RSpec.describe Focus do
+  let(:subject) { build(:focus) }
+
   it { should respond_to(:label) }
   it { should respond_to(:note) }
   it { should respond_to(:position) }
+
+  describe '#label' do
+    it { is_expected(subject.label).to be_a(String) }
+
+    it 'should not support string longer than 255 characters' do
+      expect { subject.update_column(:label, Faker::Lorem.characters(256)) }.
+        to raise_error(StandardError)
+    end
+  end
+
+  describe '#note' do
+    it { is_expected(subject.note).to be_a(String) }
+
+    it 'should support string longer than 255 characters' do
+      expect { subject.update_column(:note, Faker::Lorem.characters(256)) }.
+        to raise_error(StandardError)
+    end
+  end
+
+  describe '#position' do
+    it { is_expected(subject.position).to be_an(Integer) }
+    it { is_expected { subject.position = -1 }.to raise_error }
+  end
 end
