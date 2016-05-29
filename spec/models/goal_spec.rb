3a339d6215f07ef66b57ccc4b911374408b096c8
Edward Minnett <ed@methodic.io>
2016-05-29 14:26:41 +0100

    Fix: Correct the mixup between the Goal and Task specs
    Connects to #19

--- spec/models/goal_spec.rb
+++ spec/models/goal_spec.rb
@@ -8,8 +8,11 @@ RSpec.describe Goal do
   it { should respond_to(:note) }
   it { should respond_to(:position) }
   it { should respond_to(:due_at) }
-  it { should respond_to(:reminder_at) }
-  it { should respond_to(:repeat_frequency) }
+  it { should respond_to(:specific) }
+  it { should respond_to(:measurable) }
+  it { should respond_to(:attainable) }
+  it { should respond_to(:releavant) }
+  it { should respond_to(:timely) }
   it { should respond_to(:complete) }
   it { should respond_to(:difficulty) }
   it { should respond_to(:importance) }
