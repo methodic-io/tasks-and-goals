bc30e1f6629f95a9bf0a5f1280cc65f5e058b076
Edward Minnett <ed@methodic.io>
2016-05-30 18:18:55 +0100

    Test: Exclude join models from required date attribute tests
    Connects to #19

--- spec/models/all_core_models_spec.rb
+++ spec/models/all_core_models_spec.rb
@@ -3,14 +3,15 @@
 
 require 'rails_helper'
 
-RSpec.describe 'All Models' do
+RSpec.describe 'All Core Models' do
   Rails.application.eager_load!
 
   required_attributes = [:created_at, :updated_at, :deleted_at]
   all_models          = ActiveRecord::Base.descendants
-  user_created_models = all_models - [ActiveRecord::SchemaMigration]
+  non_core_models     = [ActiveRecord::SchemaMigration, Grouping, Listing]
+  core_models         = all_models - non_core_models
 
-  user_created_models.each do |model|
+  core_models.each do |model|
     describe model do
       it { should respond_to(*required_attributes) }
     end
