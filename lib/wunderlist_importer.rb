030cb1dfbee9b2e5d9f0d92eacfea56327d8e85c
Edward Minnett <ed@methodic.io>
2016-06-04 18:27:05 +0100

    Feat: Create the WunderlistImporter class
    Connects to #48

---
+++ lib/wunderlist_importer.rb
@@ -0,0 +1,15 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
+# A utility that takes the locates of a Wunderlist export JSON file,
+# parses its content, amnd imports the content into the DB.
+class WunderlistImporter
+  attr_reader :input
+
+  def initialize(export_file)
+    @input = JSON.parse(File.read(export_file))
+  end
+
+  def import
+  end
+end
