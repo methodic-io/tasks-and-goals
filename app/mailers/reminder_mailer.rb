e54c80d11528854d1aeb9a6fac690f9a82682fcc
Edward Minnett <ed@methodic.io>
2016-06-09 21:07:47 +0100

    Feat: Add ReminderMailer reminder_email method
    Connects to #76
    Connects to #77

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -3,4 +3,7 @@
 
 # The ReminderMailer handles Task reminder emails.
 class ReminderMailer < ApplicationMailer
+  def reminder_email
+    mail(to: 'ed@methodic.io', subject: 'Task & Goals Reminder')
+  end
 end
