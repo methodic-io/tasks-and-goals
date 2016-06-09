14b392dfb683d0fc7174ca8b7f88ce33a4df3902
Edward Minnett <ed@methodic.io>
2016-06-09 20:57:29 +0100

    Style: Add missing frozen string literal comment

--- spec/mailers/previews/reminder_mailer_preview.rb
+++ spec/mailers/previews/reminder_mailer_preview.rb
@@ -1,3 +1,6 @@
+# encoding: utf-8
+# frozen_string_literal: true
+
 # Preview all emails at http://localhost:3000/rails/mailers/reminder_mailer
 class ReminderMailerPreview < ActionMailer::Preview
 
--- spec/mailers/reminder_mailer_spec.rb
+++ spec/mailers/reminder_mailer_spec.rb
@@ -1,4 +1,7 @@
 require "rails_helper"
+# encoding: utf-8
+# frozen_string_literal: true
+
 
 RSpec.describe ReminderMailer, type: :mailer do
   pending "add some examples to (or delete) #{__FILE__}"
