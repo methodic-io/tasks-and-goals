e129cbacd9cea5ccbac2801160022dd2a8970f6c
Edward Minnett <ed@methodic.io>
2016-06-09 21:02:57 +0100

    Docs: Add top level class comments

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
+# Encapsulates general mailer methods for the Tasks & Goals application.
 class ApplicationMailer < ActionMailer::Base
   default from: 'from@example.com'
   layout 'mailer'
--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -1,5 +1,6 @@
 # encoding: utf-8
 # frozen_string_literal: true
 
+# The ReminderMailer handles Task reminder emails.
 class ReminderMailer < ApplicationMailer
 end
