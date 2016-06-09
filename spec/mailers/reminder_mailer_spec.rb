990fc1ae8a811f06d62ad80a5a16790a85a7dd59
Edward Minnett <ed@methodic.io>
2016-06-09 21:01:18 +0100

    Style: Use ' not " for simple strings

--- spec/mailers/reminder_mailer_spec.rb
+++ spec/mailers/reminder_mailer_spec.rb
@@ -1,7 +1,7 @@
-require "rails_helper"
 # encoding: utf-8
 # frozen_string_literal: true
 
+require 'rails_helper'
 
 RSpec.describe ReminderMailer, type: :mailer do
   pending "add some examples to (or delete) #{__FILE__}"
