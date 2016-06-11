3fad3eea299678daae9cce99adfc253f93c7ddc6
Edward Minnett <ed@methodic.io>
2016-06-11 13:27:59 +0100

    Style: Use the : hash syntax instead of =>

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ require 'resque/server'
 
 Rails.application.routes.draw do
 
-  mount Resque::Server.new, :at => "/resque"
+  mount Resque::Server.new, at: '/resque'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
