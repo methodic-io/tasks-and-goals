1aaacd37a759d474f002ca0cc7f7a1e7c8f6aa27
Edward Minnett <ed@methodic.io>
2016-06-11 22:45:35 +0100

    Refactor: Modify the Reminder class to pass the tests
    Connected to #79
    Connected to #80

--- lib/reminder.rb
+++ lib/reminder.rb
@@ -4,9 +4,10 @@
 # The Reminder acts as an interface between the Task and the ReminderJob
 # ensuring that only tasks that need reminders receive them.
 class Reminder
-  def initialize(task)
+  def self.setup(task)
     unless task.reminder_at.blank? || task.reminder_at.past?
-      Resque.enqueue_at(task.reminder_at, ReminderJob, task.id)
+      return Resque.enqueue_at(task.reminder_at, ReminderJob, task.id)
     end
+    :this_task_does_not_need_a_reminder
   end
 end
