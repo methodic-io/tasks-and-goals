67cc236ceeb4e95a460aa65eda9f2eb8037f0b08
Edward Minnett <ed@methodic.io>
2016-06-11 16:07:16 +0100

    Feat: Update the ReminderEmail to include a default 'from' address
    Connects to #76
    Connects to #77

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -3,6 +3,8 @@
 
 # The ReminderMailer handles Task reminder emails.
 class ReminderMailer < ApplicationMailer
+  default from: 'Methodic Automated Emails <notifications@methodic.io>'
+
   def reminder_email(task)
     mail(to: 'ed@methodic.io', subject: "Task & Goals Reminder: #{task.label}")
   end
