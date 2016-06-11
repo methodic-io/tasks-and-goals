560b77b68b9791c20c8c99a5a02183d5074eaa8f
Edward Minnett <ed@methodic.io>
2016-06-11 17:20:43 +0100

    Refactor: Assign instance variables to support dynamic email templates
    Connects to #76
    Connects to #77

--- app/mailers/reminder_mailer.rb
+++ app/mailers/reminder_mailer.rb
@@ -6,6 +6,7 @@ class ReminderMailer < ApplicationMailer
   default from: 'Methodic Automated Emails <notifications@methodic.io>'
 
   def reminder_email(task)
-    mail(to: 'ed@methodic.io', subject: "Task & Goals Reminder: #{task.label}")
+    @task = task
+    mail(to: 'ed@methodic.io', subject: "Task & Goals Reminder: #{@task.label}")
   end
 end
