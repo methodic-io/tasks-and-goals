8b4f8abda5e93c2d8a6c007e3205b6b57cbac17e
Edward Minnett <ed@methodic.io>
2016-06-15 08:52:10 +0100

    Styling: Add space inside curly brace

--- spec/factories/group.rb
+++ spec/factories/group.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
     positions = (1..10).to_a
 
     label          { generate(:label) }
-    open           { Faker::Boolean.boolean}
+    open           { Faker::Boolean.boolean }
     position       { Faker::Number.digit }
     list_positions positions
   end
