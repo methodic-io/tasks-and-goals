8f520409a3711d04ec070ab2b58b7c8582ee89fe
Edward Minnett <ed@methodic.io>
2016-06-18 11:27:56 +0100

    Feat: Add subtask positioning logic to Task
    Closes #105
    Closes #108

--- app/models/task.rb
+++ app/models/task.rb
@@ -14,12 +14,82 @@ class Task < ActiveRecord::Base
 
   has_many :listings
   has_many :lists, through: :listings
-  has_many :subtasks
+  has_many :subtasks do
+    def <<(value)
+      super(value)
+      value = [value] unless value.is_a? Array
+      proxy_association.owner.subtask_positions =
+        value.map(&:id) + proxy_association.owner.subtask_positions
+    end
+
+    def delete(value)
+      super(value)
+      proxy_association.owner.subtask_positions.delete(value.id)
+    end
+  end
 
   serialize :repeat_frequency,  Hash
   serialize :subtask_positions, Array
 
   def needs_reminding?
     !reminder_at.blank? && reminder_at.future?
+  end
+
+  def subtasks=(subtasks)
+    self.subtask_positions = subtasks.map(&:id)
+    super(subtasks)
+  end
+
+  def position_subtask(subtask, position)
+    ensure_arguments_are_subtasks(__method__, subtask)
+    ensure_subtasks_are_associated(__method__, subtask)
+    ensure_position_is_in_bounds(__method__, position)
+
+    subtask_positions.delete(subtask.id)
+    subtask_positions.insert(position, subtask.id)
+  end
+
+  def exchange_positions(subtask_a, subtask_b)
+    ensure_arguments_are_subtasks(__method__, subtask_a, subtask_b)
+    ensure_subtasks_are_associated(__method__, subtask_a, subtask_b)
+
+    pos_a = subtask_positions.index(subtask_a.id)
+    pos_b = subtask_positions.index(subtask_b.id)
+    subtask_positions[pos_a] = subtask_b.id
+    subtask_positions[pos_b] = subtask_a.id
+  end
+
+  def ordered_subtasks
+    subtasks.sort_by do |s|
+      subtask_positions.index(s.id)
+    end
+  end
+
+  private
+
+  def ensure_arguments_are_subtasks(method, *subtasks)
+    subtasks.each do |subtask|
+      unless subtask.is_a? Subtask
+        msg = "#{self.class}##{method} can only accept Subtasks as arguments."
+        raise TypeError, msg
+      end
+    end
+  end
+
+  def ensure_subtasks_are_associated(method, *subtasks)
+    unless (subtasks.map(&:id) - self.subtasks.map(&:id)).empty?
+      msg = "#{self.class} must be associated with the given subtask(s) for " \
+            "#{method} to work."
+      raise ArgumentError, msg
+    end
+  end
+
+  def ensure_position_is_in_bounds(method, pos)
+    if pos >= subtask_positions.count || pos < -subtask_positions.count
+      msg = "The position,#{pos} , must be in bounds (positive or negative) " \
+            "for #{method} to work. The positions array has a count of "      \
+            "#{subtask_positions.count}."
+      raise ArgumentError, msg
+    end
   end
 end
