ad0c3eaf8c0ef18a1e40f948dc8cb52e992f6b0d
Edward Minnett <ed@methodic.io>
2016-06-18 18:26:02 +0100

    Feat: Add the ability to 'deep clone' a Task
    Closes #100

--- lib/task_cloner.rb
+++ lib/task_cloner.rb
@@ -3,7 +3,42 @@
 
 # Deep clones a given task. This includes attributes and associations.
 class TaskCloner
-  def self.clone(_)
-    Task.new
+  def self.clone(task)
+    handle_invalid_type(task) unless task.is_a? Task
+
+    clone = task.dup
+    clone.deferred_at  = []
+    clone.completed_at = nil
+    clone.save!
+    handle_lists(clone, task)    if task.lists.any?
+    handle_subtasks(clone, task) if task.subtasks.any?
+    clone
+  end
+
+  private_class_method
+
+  def self.handle_invalid_type(task)
+    error_msg = "#{self.class} can only apply clone to Tasks. " \
+                "#{task.class} is not an instance of Task."
+    raise TypeError, error_msg
+  end
+
+  def self.handle_lists(clone, task)
+    task.lists.each do |list|
+      list.tasks << clone
+      list.exchange_positions(task, clone)
+      list.save!
+    end
+    clone.save!
+  end
+
+  def self.handle_subtasks(clone, task)
+    task.ordered_subtasks.reverse.each do |subtask|
+      new_subtask = subtask.dup
+      new_subtask.completed_at = nil
+      clone.subtasks << new_subtask
+      new_subtask.save!
+    end
+    clone.save!
   end
 end
