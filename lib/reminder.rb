83edabdfce652eb783af96c9c6d0532f36ec1363
Edward Minnett <ed@methodic.io>
2016-06-19 18:01:19 +0100

    Feat: Update Reminder to meet test expectations
    Connects to #86
    Connects to #87
    Connects to #88

--- lib/reminder.rb
+++ lib/reminder.rb
@@ -6,8 +6,16 @@
 class Reminder
   def self.setup(task)
     if task.needs_reminding?
-      return Resque.enqueue_at(task.reminder_at, ReminderJob, task.id)
+      return :this_task_needs_reminding_but_lacks_an_id if task.id.blank?
+      return Resque.enqueue_at(task.reminder_at, ReminderJob, task_id: task.id)
     end
     :this_task_does_not_need_a_reminder
+  end
+
+  def self.clear(task)
+    if task.needs_reminding?
+      return Resque.remove_delayed(ReminderJob, task_id: task.id)
+    end
+    :this_task_does_not_have_a_reminder_to_clear
   end
 end
